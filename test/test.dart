ba94b1157fdc26c157f5029b4a3aa546d5723552
Loay Mohamed <loaymohamed1999@gmail.com>
2021-09-29 01:13:03 +0200

    Unit Tests are Added for all API functions

--- test/test.dart
+++ test/test.dart
@@ -6,12 +6,18 @@ void main(List<String> args) {
   //Testing read JSON Function::
   //
   String? topId = readJSON("top1.json");
+  readJSON("top33333.json");
+  readJSON("top44.json");
   test('Testing read JSON - File Exists', () {
     expect("top1", topId);
   });
   String? topId2 = readJSON("top4.json");
   test('Testing read JSON - File Doesnot Exist', () {
     expect(null, topId2);
+  });
+  String? topId3 = readJSON("top1.json");
+  test('Testing read JSON - File Existed but read before', () {
+    expect("top1", topId3);
   });
 
   //
@@ -34,8 +40,8 @@ void main(List<String> args) {
   //Testing query Topologies Function::
   //
   Map<String, Topology> tops = queryTopologies();
-  test('Testing query Topologies Function - There is topologies list.', () {
-    expect(1, tops.length);
+  test('Testing query Topologies Function - There is a topologies list.', () {
+    expect(3, tops.length);
     expect(true, tops.isNotEmpty);
   });
 
@@ -53,5 +59,57 @@ void main(List<String> args) {
   bool resultDelete3 = writeJSON("top111");
   test('Testing delete Topology - Topology ID Doesnot Exist', () {
     expect(false, resultDelete3);
+  });
+
+  //
+  //Testing query Devices of Specific Topology Function::
+  //
+  List<Component> devices = queryDevices("top1");
+  test('Testing query Topologies Function - TopologyID existed.', () {
+    expect(2, devices.length);
+    expect(true, devices.isNotEmpty);
+  });
+  List<Component> devices2 = queryDevices("top111");
+  test('Testing query Topologies Function -  TopologyID doesnot exist.', () {
+    expect(0, devices2.length);
+    expect(false, devices2.isNotEmpty);
+  });
+  List<Component> devices3 = queryDevices(null);
+  test('Testing query Topologies Function -  TopologyID is Null.', () {
+    expect(0, devices3.length);
+    expect(false, devices3.isNotEmpty);
+  });
+
+  //
+  //Testing query Devices of Specific Topology & Netlist Function::
+  //
+  List<Component> devicesNetlist = queryDevicesWithNetlistNode("top1", "n1");
+  test(
+      'Testing query Topologies Function - TopologyID and NetlistID both existed.',
+      () {
+    expect(2, devicesNetlist.length);
+    expect(true, devicesNetlist.isNotEmpty);
+  });
+  List<Component> devicesNetlist2 = queryDevicesWithNetlistNode("top111", "n1");
+  test(
+      'Testing query Topologies Function -  TopologyID doesnot exist whatever NodeListID.',
+      () {
+    expect(0, devicesNetlist2.length);
+    expect(false, devicesNetlist2.isNotEmpty);
+  });
+  List<Component> devicesNetlist22 =
+      queryDevicesWithNetlistNode("top1", "nnn1");
+  test(
+      'Testing query Topologies Function -  TopologyID exist but NodeListID doesnot.',
+      () {
+    expect(0, devicesNetlist22.length);
+    expect(false, devicesNetlist22.isNotEmpty);
+  });
+  List<Component> devicesNetlist3 = queryDevicesWithNetlistNode(null, null);
+  test(
+      'Testing query Topologies Function -  TopologyID / NetlistID either is Null.',
+      () {
+    expect(0, devicesNetlist3.length);
+    expect(false, devicesNetlist3.isNotEmpty);
   });
 }
